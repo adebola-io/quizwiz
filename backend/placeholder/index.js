7c5d82895126890d16e6cb551439d98d555b9f8b
adebola-xyz <adebolaakomolafe@gmail.com>
2023-07-24 13:05:27 +0100

    Replace Javascript with Typescript

--- backend/placeholder/index.js
+++ backend/placeholder/index.js
@@ -1,4 +1,14 @@
 const colors = require("colors");
 const http = require("http");
 
-console.log("Hello world.".green);
+const port = process.env.PORT || 4174;
+
+/**
+ * Server Handler
+ * @param {http.IncomingMessage} req
+ * @param {http.ServerResponse} res
+ */
+function listener(req, res) {}
+
+http.createServer(listener).listen(port);
+console.log(`Local Server is running on port ${port}.`.green);
--- frontend/src/App.jsx
+++
@@ -1,7 +0,0 @@
-import "./App.css";
-
-function App() {
-   return <div>Hello, world.</div>;
-}
-
-export default App;
