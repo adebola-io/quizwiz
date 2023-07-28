d32e639787c7dfcdaafc271594173e46b84aea2f
Alayo Ibrahim <34569440+Matec12@users.noreply.github.com>
2023-07-28 11:57:21 +0100

    chore: update

--- frontend/src/utils/jwt.ts
+++ frontend/src/utils/jwt.ts
@@ -5,7 +5,6 @@ const isValidToken = (accessToken: string) => {
   if (!accessToken) {
     return false;
   }
-
   const decoded: any = jwtDecode(accessToken);
   console.log(decoded);
   const currentTime = Date.now() / 1000;
@@ -13,26 +12,10 @@ const isValidToken = (accessToken: string) => {
   return decoded.exp > currentTime;
 };
 
-const handleTokenExpired = (exp: number) => {
-  let expiredTimer;
-
-  window.clearTimeout(expiredTimer);
-  const currentTime = Date.now();
-  const timeLeft = exp * 1000 - currentTime;
-  console.log(timeLeft);
-  expiredTimer = window.setTimeout(() => {
-    console.log("expired");
-    // You can do what ever you want here, like show a notification
-  }, timeLeft);
-};
-
 const setSession = (accessToken: string | null) => {
   if (accessToken) {
     localStorage.setItem("accessToken", accessToken);
     axios.defaults.headers.common.Authorization = `Bearer ${accessToken}`;
-    // This function below will handle when token is expired
-    const { exp }: any = jwtDecode(accessToken);
-    handleTokenExpired(exp);
   } else {
     localStorage.removeItem("accessToken");
     delete axios.defaults.headers.common.Authorization;
