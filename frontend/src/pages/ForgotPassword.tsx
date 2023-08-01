961af17cef61ca5a31831520f0f0d26b044e3ccb
adebola-xyz <adebolaakomolafe@gmail.com>
2023-08-01 20:57:14 +0100

    Update forgot password page

--- frontend/src/pages/ForgotPassword.tsx
+++ frontend/src/pages/ForgotPassword.tsx
@@ -1,43 +1,48 @@
-import { Input } from "@/components/ui"
-import { Button } from "@/components/ui"
+import { Input, Button } from "@/components/ui";
+import SignInBackground from "@/assets/signin-background.png";
 
 export default function ForgotPassword() {
-  return (
-    <main className="bg-[url('/src/assets/lines-background.png')] h-fit flex flex-col-reverse md:flex-row md:h-screen items-center">
-        <section className="flex items-center h-full md:w-[48%] w-full">
-          <div className="md:w-[80%] md:mx-auto md:h-[85%] h-[500px] shadow-components/shadow border-[6px] border-green-charcoal rounded w-full">
-            <img src="/src/assets/signin-background.png" alt="background" className=" w-full h-full rounded"/>
-          </div>
-        </section>
+   return (
+      <main className="lines h-fit flex flex-col-reverse md:flex-row md:h-screen items-center">
+         <section className="flex items-center h-full md:w-[48%] w-full">
+            <img
+               src={SignInBackground}
+               alt="background"
+               className="w-full h-[500px] object-cover rounded-[0.625rem] md:w-[80%] md:mx-auto md:h-[85%] shadow-components/shadow border-[6px] border-green-charcoal"
+            />
+         </section>
 
-        <section className="h-full flex items-center md:w-[48%] w-[100%] md:pt-0 pt-8 px-4">
+         <section className="h-full flex items-center justify-end md:w-[48%] w-[100%] md:pt-0 pt-8 px-4">
             <div className="mx-auto">
-              <p className="font-bold text-[3rem] leading-tight text-green-feldgrau font-avenir-next-lt-pro-bold">Enter the email used <br /> to verify your <br /> account.</p>
+               <p className="font-bold text-[3.25rem] leading-tight w-[70%] text-green-feldgrau font-avenir-next-lt-pro-bold">
+                  Enter the email used to verify your account.
+               </p>
 
-              <Input placeholder="email" 
-              type="email" 
-              id="email" 
-              className="w-full mt-4" 
-              containerClassName="animate-fade-in-from-right effect-item-1"/>
-              
-              <Button
-              type="submit"
-              className="w-full hover:brightness-90 animate-fade-in-from-right effect-item-3"
-              variant="override"
-              style={{
-                boxShadow: "none",
-                color: "var(--white)",
-                backgroundColor: "var(--green-feldgrau)",
-                borderWidth: "4px",
-                height: "4.125rem",
-                marginBottom: "1.36rem"
-             }}
-              
+               <Input
+                  placeholder="email"
+                  type="email"
+                  id="email"
+                  className="w-full mt-4"
+                  containerClassName="animate-fade-in-from-right w-[80%] effect-item-1"
+               />
 
-              
-              >Continue</Button>
+               <Button
+                  type="submit"
+                  className="w-[80%] hover:brightness-90 animate-fade-in-from-right effect-item-3"
+                  variant="override"
+                  style={{
+                     boxShadow: "none",
+                     color: "var(--white)",
+                     backgroundColor: "var(--green-feldgrau)",
+                     borderWidth: "4px",
+                     height: "4.125rem",
+                     marginBottom: "1.36rem"
+                  }}
+               >
+                  Continue
+               </Button>
             </div>
-        </section>
-    </main>
-  )
+         </section>
+      </main>
+   );
 }
