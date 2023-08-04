879bbdfd9b77fc36ddf9935c361e74a8193322c7
adebola-xyz <adebolaakomolafe@gmail.com>
2023-08-04 02:16:00 +0100

    Start movies-and-tv.json

--- backend/categories/info.js
+++ backend/categories/info.js
@@ -1,4 +1,4 @@
-const json = require("./history-politics-and-geography/nigerian-history.json");
+const json = require("./pop-culture/music.json");
 Object.keys(json).forEach((key, index) => {
    if (json[key] instanceof Array) {
       console.log(`Level ${index - 2}: ${json[key].length} questions`);
